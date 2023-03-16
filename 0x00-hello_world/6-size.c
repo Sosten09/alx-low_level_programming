9564e73c6ad552962bd718db57afd35dc4791d96
sosten <sostensemu09@gmail.com>
2023-03-16 06:59:24 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,6 @@ int main(void)
 	printf("size of an int: %lu bytes(s)\n", sizeof(int));
 	printf("size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("size of a long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("size of a float: %lu bytes(s)\n", sizeof(float));
+	printf("size of a float: %lu bytes(s)", sizeof(float));
 	return (0);
 }
