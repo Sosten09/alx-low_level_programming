9d6b1976a155c69247deeee0c9300912599c0a4a
sosten <sostensemu09@gmail.com>
2023-03-18 07:52:01 -0700

    numbers z

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - Enrtry point
+  *Description:'numbers'
+  *Return: always 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10 ; n++)
+
+		putchar(n + '0');
+	putchar('\n');
+	return (0);
+}
