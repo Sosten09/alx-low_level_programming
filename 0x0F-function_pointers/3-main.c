add905214baec627f5ebbb0a556fbec02276b265
sosten <sostensemu09@gmail.com>
2023-04-19 09:06:29 -0700

    try and try

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,13 +6,13 @@
  * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
- * @xy : char
+ *
  * Return: Always 0.
  */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *xy;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,24 +21,28 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
-	xy = argv[2];
+	op = argv[2];
 	num2 = atoi(argv[3]);
 
-	if (get_xy_func(xy) == NULL || xy[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((*xy == '/' && num2 == 0) ||
-	    (*xy == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", get_xy_func(xy)(num1, num2));
+	printf("%d\n", get_op_func(op)(num1, num2));
 
 	return (0);
 }
+
+
+
+
 
