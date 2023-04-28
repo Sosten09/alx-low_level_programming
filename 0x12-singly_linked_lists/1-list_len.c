b18a79785f831897985cc5f44381769f697ce5e4
sosten <sostensemu09@gmail.com>
2023-04-28 02:56:45 -0700

    list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,12 +8,13 @@
  */
 size_t list_len(const list_t *h)
 {
-	 size_t g = 0;
+	 size_t n = 0;
 
-	while (h)
-
-		g++;
-	h = h->next;
-	return (g);
+while (h)
+{
+	 n++;
+	 h = h->next;
+}
+return (n);
 }
 
