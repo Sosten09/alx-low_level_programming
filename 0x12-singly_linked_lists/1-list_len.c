bec4f2021c967b744c5bcda635f5b913191f244c
sosten <sostensemu09@gmail.com>
2023-04-28 02:40:48 -0700

    number of elements in a linked list_t list.

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements in a linked list.
+ * @h: Nointer to the list_t.
+ * Return: Number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	 size_t g = 0;
+
+	while (h)
+
+		g++;
+	h = h->next;
+	return (g);
+}
+
